3f95dcc26573971c5d435dffff5f4146dce52fcc
Alexey Gmitron <agmitron@sinergo.ru>
2020-10-02 15:34:23 +0700

    complete blog

--- app.py
+++ app.py
@@ -1,36 +1,71 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_sqlalchemy import SQLAlchemy
 from os import path
+from datetime import datetime
 
 app = Flask(__name__)
+db_path = path.join(path.dirname(__file__), 'app.db')
+db_uri = 'sqlite:///{}'.format(db_path)
+app.config['SQLALCHEMY_DATABASE_URI'] = db_uri
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////blog.db'
 db = SQLAlchemy(app)
+
 
 class BlogPost(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String(50))
     subtitle = db.Column(db.String(50))
     author = db.Column(db.String(50))
-    date_posted = db.Column(db.DateTime)
+    date_posted = db.Column(db.String)
     content = db.Column(db.Text)
 
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    posts = BlogPost.query.all()
+    return render_template('index.html', posts=posts)
+
 
 @app.route('/about')
 def about():
     return render_template('about.html')
 
-@app.route('/post')
-def post():
-    return render_template('post.html')
+
+@app.route('/post/<int:post_id>')
+def post(post_id):
+    post = BlogPost.query.filter_by(id=post_id).one()
+    return render_template('post.html', post=post)
+
 
 @app.route('/contact')
 def contact():
     return render_template('contact.html')
+
+
+@app.route('/add')
+def add():
+    return render_template('add.html')
+
+
+@app.route('/add_post', methods=['POST'])
+def add_post():
+    title = request.form['title']
+    subtitle = request.form['subtitle']
+    author = request.form['author']
+    content = request.form['content']
+
+    post = BlogPost(
+        title=title,
+        subtitle=subtitle,
+        author=author,
+        content=content,
+        date_posted=datetime.now().strftime('%B %d, %Y')
+    )
+    db.session.add(post)
+    db.session.commit()
+
+    return redirect(url_for('index'))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
